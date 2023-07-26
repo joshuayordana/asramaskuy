43de82d09f736e8315dea25da882201a796388ba
joshuayordana <joshuayordana8@gmail.com>
2023-07-26 15:30:22 +0700

    JS Register Guest

--- asset/js/register_guest.js
+++ asset/js/register_guest.js
@@ -1,24 +1,23 @@
 import { config } from "./config.js";
-const endpoint =
-  `${config.api}createNewDummy`;
+const endpoint = `${config.api}createNewDummy`;
 
 document.querySelector("form").action = endpoint;
 
 var step = 1;
-function nextStep(){
-    const isi1 = document.getElementById(`isi${step}`);
-    const isi2 = document.getElementById(`isi${step+1}`);
-    isi1.style.display = "none";
-    isi2.style.display = "block";
-    step += 1;
+function nextStep() {
+  const isi1 = document.getElementById(`isi${step}`);
+  const isi2 = document.getElementById(`isi${step + 1}`);
+  isi1.style.display = "none";
+  isi2.style.display = "block";
+  step += 1;
 }
 
-function back(){
-    step-=1;
-    const isi1 = document.getElementById(`isi${step}`);
-    const isi2 = document.getElementById(`isi${step+1}`);
-    isi2.style.display = "none";
-    isi1.style.display = "block";
+function back() {
+  step -= 1;
+  const isi1 = document.getElementById(`isi${step}`);
+  const isi2 = document.getElementById(`isi${step + 1}`);
+  isi2.style.display = "none";
+  isi1.style.display = "block";
 }
 
 const arrow_back = document.getElementById("arrow_back");
@@ -27,29 +26,32 @@ const submitButton = document.getElementById("submit");
 const form = document.getElementById("register_form");
 next1.addEventListener("click", nextStep);
 arrow_back.addEventListener("click", back);
-submitButton.addEventListener("click", ()=>{
-    //Get Data
-    const register_data = {};
-    register_data["nama"] = document.getElementById("nama").value;
-    register_data["nik"] = document.getElementById("nik_input").value;
-    register_data["tgl_lahir"] = document.getElementById("tgl_lahir").value;
-    register_data["gender"] = document.getElementById("gender").value;
-    register_data["alamat"] = document.getElementById("alamat").value;
-    register_data["email"] = document.getElementById("email").value;
-    register_data["password"] = document.getElementById("password").value;
-    register_data["no_telp"] = document.getElementById("no_telp").value;
-    console.log(JSON.stringify(register_data));
-    //Submit Data
-    fetch(endpoint, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            tgl_lahir: "2023-07-26"
-        })
-    })
-   .then(response => response.json())
-   .then(response => console.log(JSON.stringify(response)));
+submitButton.addEventListener("click", () => {
+  //Get Data
+  var register_data = {};
+  register_data["nama"] = document.getElementById("nama").value;
+  register_data["nik"] = document.getElementById("nik_input").value;
+  register_data["tgl_lahir"] = document.getElementById("tgl_lahir").value;
+  console.log(register_data["tgl_lahir"]);
+  register_data["gender"] = document.getElementById("gender").value;
+  register_data["alamat"] = document.getElementById("alamat").value;
+  register_data["email"] = document.getElementById("email").value;
+  register_data["password"] = document.getElementById("password").value;
+  register_data["no_telp"] = document.getElementById("no_telp").value;
+  console.log(JSON.stringify(register_data));
+  const formData = new URLSearchParams();
+  for (const [key, value] of Object.entries(register_data)) {
+    formData.append(key, value.toString());
+  }
+  //Submit Data
+  fetch(endpoint, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: formData.toString(),
+  })
+    .then((response) => response.json())
+    .then((response) => console.log(JSON.stringify(response)));
 });
