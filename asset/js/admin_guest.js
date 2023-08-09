4c76a7869bf09d68d5af68106be12e129b055c2e
joshuayordana <joshuayordana8@gmail.com>
2023-08-09 15:06:16 +0700

    bug fix tanggal

--- asset/js/admin_guest.js
+++ asset/js/admin_guest.js
@@ -68,11 +68,11 @@ function dateFormat(tanggal) {
 
 function dateFormatInput(tanggal) {
     var fullDate = new Date(tanggal);
-    var twoDigitMonth = fullDate.getMonth() + "";
+    var twoDigitMonth = fullDate.getMonth() + 1 + "";
     if (twoDigitMonth.length == 1) twoDigitMonth = "0" + twoDigitMonth;
     var twoDigitDate = fullDate.getDate() + "";
     if (twoDigitDate.length == 1) twoDigitDate = "0" + twoDigitDate;
-    var currentDate = fullDate.getFullYear() + "-" + twoDigitMonth + 1 + "-" + twoDigitDate;
+    var currentDate = fullDate.getFullYear() + "-" + twoDigitMonth + "-" + twoDigitDate;
     return currentDate;
 }
 
