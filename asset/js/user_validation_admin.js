122bfc8af1082aee34f540764354c61546954dbe
Joshua Yordana <joshuayordana8@gmail.com>
2023-08-22 23:47:34 +0700

    Bug fix redirect admin

---
+++ asset/js/user_validation_admin.js
@@ -0,0 +1,6 @@
+// ? ini harusnya ngambil rolenya dri local storage atau gtau dah
+let user_data = JSON.parse(window.sessionStorage.getItem("user-data"));
+if (user_data === null || user_data["id_user"] === "") {
+  window.location.href = "./index.html";
+}
+// ? ini temapt untuk cek nanti sudah login atau belum
